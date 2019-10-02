31c83b4dfd4d75063afade4aa8830624acb8487b
LeoCa35 <leocalabro30@gmail.com>
2019-10-02 18:07:58 +0200

    CHAO PESCAO

--- PracticaBot_LeoCalabro/MusicaShazam.py
+++ PracticaBot_LeoCalabro/MusicaShazam.py
@@ -4,10 +4,13 @@ bot = telebot.TeleBot("916466394:AAGPERvk72AuupiUQhsg7b5XkHnPIzY-v2I")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Leo tio eres el mejorer")
+	bot.reply_to(message, "Leo tio eres el mejor.")
 
+#@bot.message_handler(func=lambda message: True)
+#def echo_all(message):
+#	bot.reply_to(message, message.text)
 @bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+def respuestaBro(message):
+	bot.reply_to(message, + " Mate")
 
 bot.polling()
